075e051d09eede811dbf918e02d34ca92cbcd7b2
Mankirat Singh <mankirat1998@gmail.com>
2020-05-22 01:50:31 +0530

    event bus created

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser= require('body-parser');
 const {randomBytes}= require('crypto');
+const axios= require("axios");
 const cors= require('cors');
 
 const app= express();
@@ -13,7 +14,7 @@ app.get('/posts', (req,res)=> {
     res.send(posts);
 })
 
-app.post('/posts', (req,res)=>{
+app.post('/posts', async (req,res)=>{
     const id= randomBytes(4).toString('hex');
     const {title}= req.body;
 
@@ -21,8 +22,19 @@ app.post('/posts', (req,res)=>{
         id,title
     }
 
+    await axios.post('http://localhost:4005/events', {
+        type:"PostCreated",
+        data: posts[id]
+    });
+
     res.status(201).send(posts);
 })
+
+app.post('/events', (req,res)=> {
+    console.log("Recieved Event", req.body.type)
+    res.status(201).send({});
+})
+
 
 app.listen(4000,()=> {
     console.log('Listening on port 4000')
